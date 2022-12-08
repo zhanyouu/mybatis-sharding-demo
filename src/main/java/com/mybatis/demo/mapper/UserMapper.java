116cef4bd986a8d2f1f676fe046aea25d9770cad
zhanyou <you.zhan@xtransfer.cn>
2022-12-08 22:43:01 +0800

    sharding-demo

--- src/main/java/com/mybatis/demo/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.mybatis.demo.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.mybatis.demo.model.User;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface UserMapper extends BaseMapper<User> {
-}
